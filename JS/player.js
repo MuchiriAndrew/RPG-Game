08a57947c87f6cdbf6107bb913f7ffaf1febfbc8
MuchiriAndrew <kariukia225@gmail.com>
2023-03-12 22:10:00 +0300

    update

--- JS/player.js
+++ JS/player.js
@@ -10,67 +10,67 @@ function Player (classType, health, mana, strength, agility, speed) {
 }
 
 
-// let PlayerMoves = {
-//     calcAttack: function() {
-//         // who attacks first??
-//         let getPlayerSpeed = player.speed;
-//         let getEnemySpeed = enemy.speed;
-//     }
+let PlayerMoves = {
+    calcAttack: function() {
+        // who attacks first??
+        let getPlayerSpeed = player.speed;
+        let getEnemySpeed = enemy.speed;
+    }
 
-//     //Player attacks
-//         let playerAttack = function() {
+    //Player attacks
+        let playerAttack = function() {
 
-//         let calcBaseDamage;
-//         if (player.mana > 0) {
-//             calcBaseDamage = player.strength * player.mana /1000;
-//         }
-//         else{
-//             calcBaseDamage = player.strength * player.agility /1000;
-//         }
-//         let offsetDamage = Math.floor(Math.random() * Math.floor(10));
-//         let calcOutputDamage = calcBaseDamage + offsetDamage;
+        let calcBaseDamage;
+        if (player.mana > 0) {
+            calcBaseDamage = player.strength * player.mana /1000;
+        }
+        else{
+            calcBaseDamage = player.strength * player.agility /1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage;
 
-//         //no. of hits
+        //no. of hits
 
-//         let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10 / 2)) + 1;
-//         let attackValues = [calcOutputDamage, numberOfHits];
-//         return attackValues;
-//     }
+        let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10 / 2)) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    }
 
-//     let enemyAttack = function() {
-//         //enemy attacks
+    let enemyAttack = function() {
+        //enemy attacks
 
-//         let calcBaseDamage;
-//         if (player.mana > 0) {
-//             calcBaseDamage = enemy.strength * enemy.mana /1000;
-//         }
-//         else{
-//             calcBaseDamage = enemy.strength * enemy.agility /1000;
-//         }
-//         let offsetDamage = Math.floor(Math.random() * Math.floor(10));
-//         let calcOutputDamage = calcBaseDamage + offsetDamage;
+        let calcBaseDamage;
+        if (player.mana > 0) {
+            calcBaseDamage = enemy.strength * enemy.mana /1000;
+        }
+        else{
+            calcBaseDamage = enemy.strength * enemy.agility /1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage;
 
-//         //no. of hits
+        //no. of hits
 
-//         let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10 / 2)) + 1;
-//         let attackValues = [calcOutputDamage, numberOfHits];
-//         return attackValues;
-//     }
-// //get player/enemy health to change later
-//     let getPlayerHealth = document.querySelector(".health-player");
-//     let getEnemyHealth = document.querySelector(".health-enemy");
+        let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10 / 2)) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    }
+//get player/enemy health to change later
+    let getPlayerHealth = document.querySelector(".health-player");
+    let getEnemyHealth = document.querySelector(".health-enemy");
 
-//     //initiate attacks!!
+    //initiate attacks!!
 
-//     if (getPlayerSpeed >= getEnemySpeed) {
-//         let playerAttackValues = playerAttack();
-//         let totalDamage = playerAttackValues[0] * playerAttackValues[1];
-//         enemy.health = enemy.health - totalDamage;
+    if (getPlayerSpeed >= getEnemySpeed) {
+        let playerAttackValues = playerAttack();
+        let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+        enemy.health = enemy.health - totalDamage;
 
-//         alert("You hit " + playerAttackValues[0] + "damage" + playerAttackValues[1] + "times.");
-//         if (enemy.health<=0) {
-//             alert("You win!!Refresh the browser to play again");
-//         }
-//     }
-// }
+        alert("You hit " + playerAttackValues[0] + "damage" + playerAttackValues[1] + "times.");
+        if (enemy.health<=0) {
+            alert("You win!!Refresh the browser to play again");
+        }
+    }
+}
 
