a60f37c4de2ab51c598f44d00fe2ee9f8f889ab5
MuchiriAndrew <kariukia225@gmail.com>
2023-04-04 12:48:53 +0300

    updates

--- JS/player.js
+++ JS/player.js
@@ -36,8 +36,8 @@ let PlayerMoves = {
         }
 
 
-        let offsetDamage = Math.floor(Math.random() * Math.floor(10));//OffsetDamage will define luck determined damage thus we generate a random number
-        let calcOutputDamage = calcBaseDamage + offsetDamage;//OutputDamage will be the official damage that we deal to the enemy
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10)); //OffsetDamage will define luck determined damage thus we generate a random number
+        let calcOutputDamage = calcBaseDamage + offsetDamage; //OutputDamage will be the official damage that we deal to the enemy
 
         //no. of hits
 
@@ -79,11 +79,11 @@ let PlayerMoves = {
         let totalDamage = playerAttackValues[0] * playerAttackValues[1];
         enemy.health = enemy.health - totalDamage;
 
-        alert("You hit  " + playerAttackValues[0] + "damage  " + playerAttackValues[1] + "times.");
+        alert("You hit " + playerAttackValues[0] + " damage " + playerAttackValues[1] + " times.");
 
         //check if enemy is still alive    
         if (enemy.health<=0) {
-            alert("You win!!Refresh the browser to play again");
+            alert("You win!! Refresh the browser to play again");
             getEnemyHealth.innerHTML = 'Health: 0 ';
             getPlayerHealth.innerHTML = 'Health: ' + player.health;
         }
@@ -95,16 +95,54 @@ let PlayerMoves = {
             let enemyAttackValues = enemyAttack();let totalDamage = playerAttackValues[0] * playerAttackValues[1];
             player.health = player.health - totalDamage;
     
-            alert("Enemy hit " + enemyAttackValues[0] + "damage" + enemyAttackValues[1] + "times.");
+            alert("Enemy hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
     
             //check if player is still alive!  
             if (player.health<=0) {
-                alert("You lose!!Refresh the browser to play again");
+                alert("You lose!! Refresh the browser to play again");
                 getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
                 getPlayerHealth.innerHTML = 'Health: 0 ' ;
             }
             else{
                 getPlayerHealth.innerHTML = 'Health: ' + player.health;
+            }
+
+        }
+    }
+
+    else if (getEnemySpeed >= getPlayerSpeed ) {
+    
+        let enemyAttackValues = enemyAttack();
+        let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+        player.health = player.health - totalDamage;
+
+        alert("Enemy hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+
+        //check if player is still alive    
+        if (player.health<=0) {
+            alert("You lose!! Refresh the browser to play again");
+            getPlayerHealth.innerHTML = 'Health: 0 ';
+            getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+        }
+        else{
+            getPlayerHealth.innerHTML = 'Health: ' + player.health;
+
+            //enemy attacks
+
+            let playerAttackValues = playerAttack();
+            let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+            enemy.health = enemy.health - totalDamage;
+    
+            alert("You hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+    
+            //check if enemy is still alive!  
+            if (enemy.health<=0) {
+                alert("You win!! Refresh the browser to play again");
+                getPlayerHealth.innerHTML = 'Health: ' + player.health;
+                getEnemyHealth.innerHTML = 'Health: 0 ' ;
+            }
+            else{
+                getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
             }
 
         }
